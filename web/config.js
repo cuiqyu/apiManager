6020452819196ba088de59310cb4d5f496bf217a
limpid <18720956785@163.com>
2017-05-10 11:55:58 +0800

    提交api文档管理系统

---
+++ web/config.js
@@ -0,0 +1,12 @@
+toastr.options.escapeHtml = true;
+toastr.options.closeButton = true;
+toastr.options.positionClass = 'toast-top-center';
+toastr.options.preventDuplicates=true;
+window._xyj_={
+    history:false,
+    version:'1.7.1',
+    api:'',
+    //api:'//localhost:9999',
+    ws:''
+};
+console.log('version:'+_xyj_.version);
---
+++ web/html/assets/js/config.js
@@ -0,0 +1,12 @@
+toastr.options.escapeHtml = true;
+toastr.options.closeButton = true;
+toastr.options.positionClass = 'toast-top-center';
+toastr.options.preventDuplicates=true;
+window._xyj_={
+    history:false,
+    version:'1.7',
+    api:'//www.xiaoyaoji.com.cn',
+    //api:'//localhost:9999',
+    ws:'ws://www.xiaoyaoji.com.cn'
+};
+console.log('version:'+_xyj_.version);
